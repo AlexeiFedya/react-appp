a83cffb8fd2f0fa608bc66743c158024563bd204
AlexeiFedya <fedya5711@icloud.com>
2020-06-18 16:09:22 +0300

    Updates

---
+++ precache-manifest.b12365f4d3884dee8ef0494c9beeb41e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7ddbd5cb07c44a4d19a3114e757b58",
+    "url": "/react-appp/index.html"
+  },
+  {
+    "revision": "feb86dbc69b45f3d377b",
+    "url": "/react-appp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "73ec337a3e16d6230ae4",
+    "url": "/react-appp/static/js/2.86d3e419.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-appp/static/js/2.86d3e419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb86dbc69b45f3d377b",
+    "url": "/react-appp/static/js/main.e5141d1f.chunk.js"
+  },
+  {
+    "revision": "c54e23c22679e1de1d25",
+    "url": "/react-appp/static/js/runtime-main.78b5e7b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-appp/static/media/logo.5d5d9eef.svg"
+  }
+]);
